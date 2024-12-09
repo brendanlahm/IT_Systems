bc358489ca3f5854be68d2686a50602d4aae041e
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2024-12-09 19:36:16 +0100

    Script now calculates average retention time on a first-come-first-serve basis & by shortest job first

--- IT_exercise4.py
+++ IT_exercise4.py
@@ -1,6 +1,6 @@
-################################################################ Exercise 4
-########################################### Task 2 - Scheduling
-### Define Jobs in a list
+##################################################################################### Exercise 4
+################################################################ Task 2 - Scheduling
+######################################### Define Jobs in a list
 Jobs = [
   {'job': 'A', 'time': 10, 'prio': 3},
   {'job': 'B', 'time': 6, 'prio': 5},
@@ -9,10 +9,10 @@ Jobs = [
   {'job': 'E', 'time': 8, 'prio': 4}
 ]
 
-### Sort Jobs by priority
+###################################### Sort Jobs by priority (priority scheduling)
 def Priority(p):
     return p['prio']
-Jobs.sort(reverse = True, key=Priority)
+Jobs.sort(reverse=True, key=Priority)
 
 ### Calculate retention times
 totals = []
@@ -26,8 +26,39 @@ for i in range(len(Jobs)):
 import statistics
 print(statistics.mean(totals))
 
+#################################### Sort Jobs by first-come-first-serve basis
+def JobValue(p):
+    return p['job']
+Jobs.sort(key=JobValue)
 
+### Calculate retention times
+totals = []
+for i in range(len(Jobs)):
+    Jobs[0].update({'total': Jobs[0]['time']})
+    if i > 0:
+        Jobs[i].update({'total': Jobs[i]['time']+Jobs[i-1]['total']})
+    totals.append(Jobs[i].get('total'))
 
+### Take mean of retention times
+import statistics
+print(statistics.mean(totals))
+
+#################################### Sort Jobs by time to completion (shortest job first)
+def Time(p):
+    return p['time']
+Jobs.sort(key=Time)
+
+### Calculate retention times
+totals = []
+for i in range(len(Jobs)):
+    Jobs[0].update({'total': Jobs[0]['time']})
+    if i > 0:
+        Jobs[i].update({'total': Jobs[i]['time']+Jobs[i-1]['total']})
+    totals.append(Jobs[i].get('total'))
+
+### Take mean of retention times
+import statistics
+print(statistics.mean(totals))
 
 
 

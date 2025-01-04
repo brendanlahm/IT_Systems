4e8b9e55d4e3c31cbf01bac2aa0fa457e576720f
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2025-01-04 10:59:12 +0100

    Ex. 7 R script plots a complex sin function

---
+++ IT_exercise7.R
@@ -0,0 +1,31 @@
+####################################################### IT Exercise 7
+### Plotting a basic sin function
+x <- seq(0,8*pi,length.out=100)
+y <- sin(x)
+plot(x,y,type="l")
+
+### Plotting a sin function for Task 1b
+x <- seq(0.8*pi,length.out=20)
+y <- 4/pi*(sin(x)+sin(3*x)/3+sin(5*x)/5+sin(7*x)/7+sin(9*x)/9)
+plot(x,y,type="l")
+
+### Natural Log
+3^4
+exp(4*log(3))
+
+### Log Base 2
+log2(3)
+log(3)/log(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Repository for learning concepts in IT Systems
 ## Exercise 1: Caches
 ## Exercise 4: Operating Systems
 ## Exercise 6: Shell Programming
+## Exercise 7: Networks
